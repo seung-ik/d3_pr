a13027a0548e412809de75148898805cf9408ac7
seung-ik <5seungik@traum.biz>
2022-03-16 18:56:33 +0900

    practice d3 draw circle by d3

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # d3_pr
+https://wattenberger.com/blog/react-and-d3
+
+day1
+svg 에 ref 를 걸어주고 해당 ref를 d3.select에 걸어주면 svg 객체에 그림 그릴수 있다.!
+해당 객체에 append('circle') 과 같이 원하는 모양 과 정보를 입려해서 구체적인 형태를 그릴수 있다.
+명령적으로 사용할수 있고 jsx 문법을 이용해 선언적으로도 사용할수 있다.
+
+d3 역할이 svg ref에 접근하고 조작을 도와주는 건가??
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import * as d3 from 'd3';
+import styled from 'styled-components';
+
+const Wrapper = styled('div')`
+  display: flex;
+  width: 100%;
+  height: 80vh;
+  padding: 2rem;
+  border: 1px solid green;
+  box-sizing: border-box;
+`;
 
 function App() {
-  return <div className="App">aaa</div>;
+  const svgRef = useRef(null);
+  const width = 600;
+  const height = 500;
+  const margin = {
+    top: 20, right: 30, bottom: 30, left: 40
+  }
+
+  React.useEffect(() => {
+    const svgElement = d3.select(svgRef.current);
+    svgElement.append('circle').attr('cx', 0).attr('cy', 0).attr('r', 50);
+  }, []);
+
+  return (
+    <Wrapper
+      className="App"
+      style={{ display: 'flex', width: '100%', height: '80vh', padding: '2rem', border: '1px solid green' }}
+    >
+      <svg
+        ref={svgRef}
+        style={{
+          border: '2px solid gold',
+        }}
+      />
+      <svg
+        style={{
+          border: '2px solid gold',
+        }}
+      />
+    </Wrapper>
+  );
 }
 
 export default App;
